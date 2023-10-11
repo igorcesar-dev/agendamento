cfb6dc9e6de579d90c1024767f028eda45200904
igorcesar-dev <igorcesar.dev@gmail.com>
2023-10-11 13:30:29 -0300

    Criando o projeto de agendamento

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+app.use(express.static("public"))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost:27017/agendamento", {useNewUrlParser: true, useUnifiedTopology: true})
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("teste");
+})
+
+app.listen(8080, () => {
+    
+ })
