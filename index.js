843b5c366cc38eea8b3ed9fed6c3678e7b89b062
igorcesar-dev <igorcesar.dev@gmail.com>
2023-10-13 12:39:04 -0300

    Adiocionando biblioteca para mascara no cpf

--- index.js
+++ index.js
@@ -2,21 +2,48 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const appointmentService = require("./services/AppointmentService");
+const AppointmentService = require("./services/AppointmentService");
 
 app.use(express.static("public"))
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost:27017/agendamento", {useNewUrlParser: true, useUnifiedTopology: true})
+app.set('view engine', 'ejs');
 
-
-
+mongoose.connect("mongodb://localhost:27017/agendamento", { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.get("/", (req, res) => {
-    res.send("teste");
+    res.render("index")
+})
+
+app.get("/cadastro", (req, res) => {
+    res.render("create");
+})
+
+app.post("/create", async (req, res) => {
+    const status = await appointmentService.Create(
+        req.body.name,
+        req.body.email,
+        req.body.description,
+        req.body.cpf,
+        req.body.date,
+        req.body.time,
+    )
+
+    if(status){
+        res.redirect("/")
+    }else{
+        res.send("Ocorreu uma falha!")
+    }
+})
+
+app.get("/getCalendar", async(req, res) => {
+    const consultas = await AppointmentService.GetAll(false);
+    res.json(consultas);
 })
 
 app.listen(8080, () => {
-    
- })
+    console.log("Rodando")
+})
---
+++ services/AppointmentService.js
@@ -0,0 +1,38 @@
+const appointment = require("../models/Appointment")
+const mongoose = require("mongoose")
+
+const Appo = mongoose.model("Appointment", appointment)
+
+class AppointmentService {
+    async Create(name, email, description, cpf, date, time, finished) {
+        const newAppo = new Appo({
+            name,
+            email,
+            description,
+            cpf,
+            date,
+            time,
+            finished: false
+        });
+
+        try {
+            await newAppo.save();
+            return true;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+
+    }
+
+    async GetAll(showFinished) {
+        if (showFinished) {
+            return await Appo.find();
+        } else {
+            return await Appo.find({ 'finished': false });
+        }
+    }
+
+}
+
+module.exports = new AppointmentService();
